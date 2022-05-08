687f1ba2b5e05db297d8e000359eda503a400c98
MoeinT <moin.torabi@gmail.com>
2022-05-08 14:07:34 +0200

    added sql_recources

---
+++ tf/resources.tf
@@ -0,0 +1,37 @@
+resource "azurerm_data_factory" "covid-reporting-df" {
+  name                = "covrepdfmoein"
+  location            = azurerm_resource_group.covid-reporting-rg.location
+  resource_group_name = azurerm_resource_group.covid-reporting-rg.name
+}
+
+resource "azurerm_storage_account" "covid-reporting-sa" {
+  name                     = "covrepsamoein"
+  resource_group_name      = azurerm_resource_group.covid-reporting-rg.name
+  location                 = azurerm_resource_group.covid-reporting-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "blob-container-population" {
+  name                  = "blobmoein"
+  storage_account_name  = azurerm_storage_account.covid-reporting-sa.name
+  container_access_type = "blob"
+}
+
+#To use this sa for dlgen2, we need to configure a hierarchical namespace
+resource "azurerm_storage_account" "covid-reporting-sa-dl" {
+  name                     = "covrepsadlmoein"
+  resource_group_name      = azurerm_resource_group.covid-reporting-rg.name
+  location                 = azurerm_resource_group.covid-reporting-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = "true"
+}
+
+
+
+
+
+
+
+
