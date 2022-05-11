e6a81b353aba322d124984460392a18cade1d0ad
MoeinT <moin.torabi@gmail.com>
2022-05-11 20:01:57 +0200

    adde a pipeline

--- tf/adf_resources.tf
+++ tf/adf_resources.tf
@@ -58,76 +58,76 @@ resource "azurerm_data_factory_pipeline" "pl_ingest_population" {
   data_factory_id = azurerm_data_factory.covid-reporting-df.id
   concurrency     = 1
 
-  activities_json = <<JSON
-[
-    {
-        "name": "pl_ingest_pop_moein",
-        "properties": {
-            "activities": [
-                {
-                    "name": "Copy Population Data",
-                    "type": "Copy",
-                    "dependsOn": [],
-                    "policy": {
-                        "timeout": "0.00:05:00",
-                        "retry": 0,
-                        "retryIntervalInSeconds": 30,
-                        "secureOutput": false,
-                        "secureInput": false
-                    },
-                    "userProperties": [],
-                    "typeProperties": {
-                        "source": {
-                            "type": "DelimitedTextSource",
-                            "storeSettings": {
-                                "type": "AzureBlobStorageReadSettings",
-                                "recursive": true,
-                                "enablePartitionDiscovery": false
-                            },
-                            "formatSettings": {
-                                "type": "DelimitedTextReadSettings"
-                            }
-                        },
-                        "sink": {
-                            "type": "DelimitedTextSink",
-                            "storeSettings": {
-                                "type": "AzureBlobFSWriteSettings"
-                            },
-                            "formatSettings": {
-                                "type": "DelimitedTextWriteSettings",
-                                "quoteAllText": true,
-                                "fileExtension": ".txt"
-                            }
-                        },
-                        "enableStaging": false,
-                        "translator": {
-                            "type": "TabularTranslator",
-                            "typeConversion": true,
-                            "typeConversionSettings": {
-                                "allowDataTruncation": true,
-                                "treatBooleanAsNumber": false
-                            }
-                        }
-                    },
-                    "inputs": [
-                        {
-                            "referenceName": "ds_population_moein_gz",
-                            "type": "DatasetReference"
-                        }
-                    ],
-                    "outputs": [
-                        {
-                            "referenceName": "ds_population_moein_tsv",
-                            "type": "DatasetReference"
-                        }
-                    ]
-                }
-            ],
-            "annotations": [],
-            "lastPublishTime": "2022-05-11T15:45:58Z"
-        },
-        "type": "Microsoft.DataFactory/factories/pipelines"
-    }
-]
-  JSON
+#   activities_json = <<JSON
+# [
+#     {
+#         "name": "pl_ingest_pop_moein",
+#         "properties": {
+#             "activities": [
+#                 {
+#                     "name": "Copy Population Data",
+#                     "type": "Copy",
+#                     "dependsOn": [],
+#                     "policy": {
+#                         "timeout": "0.00:05:00",
+#                         "retry": 0,
+#                         "retryIntervalInSeconds": 30,
+#                         "secureOutput": false,
+#                         "secureInput": false
+#                     },
+#                     "userProperties": [],
+#                     "typeProperties": {
+#                         "source": {
+#                             "type": "DelimitedTextSource",
+#                             "storeSettings": {
+#                                 "type": "AzureBlobStorageReadSettings",
+#                                 "recursive": true,
+#                                 "enablePartitionDiscovery": false
+#                             },
+#                             "formatSettings": {
+#                                 "type": "DelimitedTextReadSettings"
+#                             }
+#                         },
+#                         "sink": {
+#                             "type": "DelimitedTextSink",
+#                             "storeSettings": {
+#                                 "type": "AzureBlobFSWriteSettings"
+#                             },
+#                             "formatSettings": {
+#                                 "type": "DelimitedTextWriteSettings",
+#                                 "quoteAllText": true,
+#                                 "fileExtension": ".txt"
+#                             }
+#                         },
+#                         "enableStaging": false,
+#                         "translator": {
+#                             "type": "TabularTranslator",
+#                             "typeConversion": true,
+#                             "typeConversionSettings": {
+#                                 "allowDataTruncation": true,
+#                                 "treatBooleanAsNumber": false
+#                             }
+#                         }
+#                     },
+#                     "inputs": [
+#                         {
+#                             "referenceName": "ds_population_moein_gz",
+#                             "type": "DatasetReference"
+#                         }
+#                     ],
+#                     "outputs": [
+#                         {
+#                             "referenceName": "ds_population_moein_tsv",
+#                             "type": "DatasetReference"
+#                         }
+#                     ]
+#                 }
+#             ],
+#             "annotations": [],
+#             "lastPublishTime": "2022-05-11T15:45:58Z"
+#         },
+#         "type": "Microsoft.DataFactory/factories/pipelines"
+#     }
+# ]
+#   JSON
 }
