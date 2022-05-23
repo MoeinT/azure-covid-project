08713ee5e052a6883a36da338ac51de1855abb9a
MoeinT <moin.torabi@gmail.com>
2022-05-23 10:48:30 +0200

    Created a custom linked service pointing to an http endpoint

--- tf/locals.tf
+++ tf/locals.tf
@@ -4,4 +4,5 @@ locals {
   arm_pipeline_template = "templates/adf_pipeline.json"
   source_blob_name      = "population_by_age.tsv.gz"
   target_blob_name      = "population_by_age.tsv"
+  target_cases          = "cases_deaths_csv"
 }
