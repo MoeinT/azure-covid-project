82898f3684097bbd9b332c888ddc8376f5121d36
MoeinT <moin.torabi@gmail.com>
2022-05-27 11:36:35 +0200

    added a container for lookup deaths_cases

--- tf/adf_dataflow.tf
+++ tf/adf_dataflow.tf
@@ -1,6 +1,6 @@
 #Create a container holding the lookup file for the cases_death dataset: 
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "file-system-lookup" {
-  name               = "lookup_cases_death${local.my_name}"
+  name               = "lookup-casesdeaths"
   storage_account_id = azurerm_storage_account.covid-reporting-sa-dl.id
 }
