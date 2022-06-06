d3cc7d87ec8fa6579436c68d30279453fac64c36
MoeinT <moin.torabi@gmail.com>
2022-06-06 13:45:55 +0200

    Saving to the target destination

--- Scripts/PySpark_transformations.py
+++ Scripts/PySpark_transformations.py
@@ -28,7 +28,6 @@ def transform_hospitals(df):
 
 def load(path_source, path_target):
     df_hospitals_processed = transform_hospitals(extract(path_source))
-    # df_hospitals_processed.write.format("csv").options("header",True).save(path_target)
     df_hospitals_processed.write.csv(path_target)
 
 
