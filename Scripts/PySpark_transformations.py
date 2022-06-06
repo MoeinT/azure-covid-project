2bcdc062d38ab11e1cd41a653f25f5a0d63d3d20
MoeinT <moin.torabi@gmail.com>
2022-06-06 12:48:10 +0200

    added a workflow to test PySpark

--- Scripts/PySpark_transformations.py
+++ Scripts/PySpark_transformations.py
@@ -7,17 +7,24 @@ from pyspark.sql.types import *
 
 spark = SparkSession.builder.appName("azure-covid-project").getOrCreate()
 
+
 def extract(path):
-    return spark.read.csv(path, header = True, inferSchema=True)
+    return spark.read.csv(path, header=True, inferSchema=True)
 
 
 def transform_hospitals(df):
     return (
-        df.withColumn("year", split("year_week", "-").getItem(0).cast(IntegerType())) \
-            .withColumn("week", substring(split("year_week", "-").getItem(1), 2, 3).cast(IntegerType())) \
-                .where(~col("indicator").contains("Weekly")) \
-                    .groupBy("country", "date", "year", "week", "source").pivot("indicator").agg(sum("value"))
+        df.withColumn("year", split("year_week", "-").getItem(0).cast(IntegerType()))
+        .withColumn(
+            "week",
+            substring(split("year_week", "-").getItem(1), 2, 3).cast(IntegerType()),
+        )
+        .where(~col("indicator").contains("Weekly"))
+        .groupBy("country", "date", "year", "week", "source")
+        .pivot("indicator")
+        .agg(sum("value"))
     )
+
 
 def get_date(path_source):
 
@@ -25,6 +32,7 @@ def get_date(path_source):
 
 
 if __name__ == "__main__":
-    df_hospitals_processed = get_date(os.path.join("..", "data", "raw", "hospitals_admissions.csv"))
+    df_hospitals_processed = get_date(
+        os.path.join("..", "data", "raw", "hospitals_admissions.csv")
+    )
     print("Ran successfully!")
-
