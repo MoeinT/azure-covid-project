ba9669a9ec1d4cabc71f7f322c7c809b6da0dcee
MoeinT <moin.torabi@gmail.com>
2022-06-07 18:49:01 +0200

    added datasets for sink

--- tf/adf_transformation_hospital.tf
+++ tf/adf_transformation_hospital.tf
@@ -15,14 +15,15 @@ resource "azurerm_data_factory_dataset_delimited_text" "ds-hospitals-admissions"
   row_delimiter       = "\n"
 }
 
-resource "azurerm_data_factory_dataset_delimited_text" "ds-hospitals-admissions-processed" {
-  name                = "ds_processed_hospital_admissions_${local.my_name}"
+#Dataset to store the hospital admissions data on a weekly basis
+resource "azurerm_data_factory_dataset_delimited_text" "ds-hospitals-admissions-processed-weekly" {
+  name                = "ds_processed_hospital_admissions_weekly${local.my_name}"
   data_factory_id     = azurerm_data_factory.covid-reporting-df.id
   linked_service_name = azurerm_data_factory_linked_service_data_lake_storage_gen2.adf-link-target.name
 
   azure_blob_fs_location {
     file_system = azurerm_storage_data_lake_gen2_filesystem.file-system-processed.name
-    path        = "ecdc/hospital_admissions"
+    path        = "ecdc/hospital_admissions_weekly"
   }
   first_row_as_header = true
   compression_level   = "Optimal"
@@ -30,6 +31,39 @@ resource "azurerm_data_factory_dataset_delimited_text" "ds-hospitals-admissions-
   row_delimiter       = "\n"
 }
 
+#Dataset to store the hospital admissions data on a daily basis
+resource "azurerm_data_factory_dataset_delimited_text" "ds-hospitals-admissions-processed-daily" {
+  name                = "ds_processed_hospital_admissions_daily${local.my_name}"
+  data_factory_id     = azurerm_data_factory.covid-reporting-df.id
+  linked_service_name = azurerm_data_factory_linked_service_data_lake_storage_gen2.adf-link-target.name
+
+  azure_blob_fs_location {
+    file_system = azurerm_storage_data_lake_gen2_filesystem.file-system-processed.name
+    path        = "ecdc/hospital_admissions_daily"
+  }
+  first_row_as_header = true
+  compression_level   = "Optimal"
+  column_delimiter    = ","
+  row_delimiter       = "\n"
+}
+
+
+#Create a dataset that points towards the Dim_Date lookup file
+resource "azurerm_data_factory_dataset_delimited_text" "ds-lookup-dim-date" {
+  name                = "ds_dim_date_${local.my_name}"
+  data_factory_id     = azurerm_data_factory.covid-reporting-df.id
+  linked_service_name = azurerm_data_factory_linked_service_data_lake_storage_gen2.adf-link-target.name
+
+  azure_blob_fs_location {
+    file_system = azurerm_storage_data_lake_gen2_filesystem.file-system-lookup.name
+    #path        = ""
+    filename = local.lookup_file_dim_date
+  }
+  first_row_as_header = true
+  compression_level   = "Optimal"
+  column_delimiter    = ","
+  row_delimiter       = "\n"
+}
 
 # data "template_file" "pipelines-dataflow-hospitals-admissions" {
 #   template = file(local.arm_pipeline_dataflow_hospitals)
