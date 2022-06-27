e10e4384db00d2f9eb32596cb2fa04362db2062d
MoeinT <moin.torabi@gmail.com>
2022-06-27 08:30:24 +0200

    fixed the access issues with databricks

--- tf/databricks.tf
+++ tf/databricks.tf
@@ -15,7 +15,7 @@ data "databricks_spark_version" "latest_lts" {
   long_term_support = true
 }
 
-#Databricks cluster
+#Databricks cluster (permissions added)
 resource "databricks_cluster" "Databricks_cluster" {
   cluster_name            = "covid-reporting-cluster-${local.my_name}"
   spark_version           = data.databricks_spark_version.latest_lts.id
