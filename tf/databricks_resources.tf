7b4fe063456264d07b86c25bcc7a6ad1a7fef570
MoeinT <moin.torabi@gmail.com>
2022-07-12 11:00:43 +0200

    added db token

--- tf/databricks_resources.tf
+++ tf/databricks_resources.tf
@@ -31,8 +31,9 @@ resource "azurerm_databricks_workspace" "covid-rp-databricks" {
 
 #We need to reference an existing workspace in our Databricks provider
 provider "databricks" {
-  host  = azurerm_databricks_workspace.covid-rp-databricks.workspace_url
-  token = "dapif7ccc464b6dd2577615f537d29bcf827-3" #var.db_access_token
+  host      = azurerm_databricks_workspace.covid-rp-databricks.workspace_url
+  token     = var.db_access_token
+  auth_type = "pat"
 }
 
 #Spark version
