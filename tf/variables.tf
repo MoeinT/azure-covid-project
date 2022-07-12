8748cc0f1262c9db187b58079e567898d58c4772
MoeinT <moin.torabi@gmail.com>
2022-07-12 10:34:15 +0200

    added db token

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,23 +1,7 @@
-variable "mssql_login" {
-  description = "Database administrator login"
-  type        = string
-  sensitive   = true
-}
-
-variable "mssql_password" {
-  description = "Database administrator password"
-  type        = string
-  sensitive   = true
-}
-
 variable "db_access_token" {
   type      = string
   sensitive = true
 }
-
-# variable "snowflake_password" {
-#   type = string 
-# }
 
 variable "snowflake_username" {
   type    = string
